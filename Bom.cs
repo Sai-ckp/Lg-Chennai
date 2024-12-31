c161feec0dd9122d8b5449d16e600a55ba852681
Sai-ckp <saishashank@ckpsoftware.com>
2024-12-31 16:21:14 +0530

    Add files via upload

---
+++ Bom.cs
@@ -0,0 +1,16 @@
+﻿namespace ERP_API.Data
+{
+    public class Bom
+    {
+        
+        public int? Id {  get; set; }
+
+        public int? ItemId { get; set; }
+
+        public  double? RMUtilityqty { get; set; }
+
+        public Item Item { get; set; }
+
+
+    }
+}
---
+++ BomReadonlyDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ERP_API.Moduls
+{
+    public class BomReadonlyDto
+    {
+        public int? Id { get; set; }
+
+        public int? ItemId { get; set; }
+
+        public double? RMUtilityqty { get; set; }
+    }
+}
